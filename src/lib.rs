179ef8af521082a83722616f358f99961ddfb504
Nick Fitzgerald <fitzgen@gmail.com>
2016-07-28 10:56:07 -0700

    Add example usage to the top level crate documentation
    Fixes #6

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,64 @@
-//! A parser for the DWARF debugging information format.
+//! A lazy, zero-copy parser for the DWARF debugging information format.
 //!
-//! This library targets the fourth edition of the standard (the most recent, at
-//! time of writing).
+//! * Zero-copy: everything is just a reference to the original input buffer. No
+//!   copies of the input data ever get made.
 //!
-//! TODO FITZGEN: example usage here!
+//! * Lazy: only the compilation units' entries that you iterate over get
+//!   parsed, and only as deep as you ask. Skip over a compilation unit and its
+//!   entries don't get parsed.
+//!
+//! * Bring your own object file parser: `gimli` isn't coupled to any platform
+//!   or object file format. Use your own ELF parser on Linux or a Mach-O parser
+//!   on OSX.
+//!
+//! This library primarily targets the fourth edition of the standard (the most
+//! recent, at time of writing).
+//!
+//! ## Example Usage
+//!
+//! Print out all of the functions in a compilation unit.
+//!
+//! ```rust,no_run
+//! use gimli;
+//!
+//! # let debug_info_buf = [];
+//! # let debug_abbrev_buf = [];
+//! # let read_debug_info = || &debug_info_buf;
+//! # let read_debug_abbrev = || &debug_abbrev_buf;
+//! // Read the .debug_info and .debug_abbrev sections with whatever object
+//! // loader you're using.
+//! let debug_info = gimli::DebugInfo::<gimli::LittleEndian>::new(read_debug_info());
+//! let debug_abbrev = gimli::DebugAbbrev::<gimli::LittleEndian>::new(read_debug_abbrev());
+//!
+//! // Grab the first compilation unit.
+//! let unit = debug_info.units().next()
+//!     .expect("Should have at least one unit")
+//!     .expect("and it should parse OK");
+//!
+//! // Parse the abbreviations for this compilation unit.
+//! let abbrevs = unit.abbreviations(debug_abbrev)
+//!     .expect("Should parse the abbreviations OK");
+//!
+//! // Get a cursor for iterating over this unit's entries.
+//! let mut entries = unit.entries(&abbrevs);
+//!
+//! // Keep iterating entries while the cursor is not exhausted.
+//! while let Some(entry) = entries.current() {
+//!     let entry = entry.expect("Should parse the entry OK");
+//!
+//!     // If we find an entry for a function, print it.
+//!     if entry.tag() == gimli::DW_TAG_subprogram {
+//!         println!("Found a function: {:?}", entry);
+//!     }
+//!
+//!     // Advance the cursor to the next entry, in DFS order.
+//!     entries.next_dfs();
+//! }
+//! ```
+//!
+//! See the
+//! [`examples/dwarfdump.rs`](https://github.com/fitzgen/gimli/blob/master/examples/dwarfdump.rs)
+//! program for a complete example program.
 
 #![deny(missing_docs)]
 
