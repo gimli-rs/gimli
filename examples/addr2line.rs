da3c82010107a9dac69a20c2dd573b1ce64e0589
Nick Fitzgerald <fitzgen@gmail.com>
2016-08-19 13:48:42 -0700

    Instead of copy-pasting platform specific obj modules, use the object crate
    The object crate is identical, but we don't have copy-paste all over the place
    anymore.

--- examples/addr2line.rs
+++ examples/addr2line.rs
@@ -1,5 +1,6 @@
 extern crate gimli;
 extern crate getopts;
+extern crate object;
 
 use std::env;
 
@@ -13,8 +14,8 @@ fn main() {
 
     let matches = opts.parse(&args[1..]).unwrap();
     let file_path = matches.opt_str("e").unwrap_or("a.out".to_string());
-    let file = obj::open(&file_path);
-    if obj::is_little_endian(&file) {
+    let file = object::open(&file_path);
+    if object::is_little_endian(&file) {
         symbolicate::<gimli::LittleEndian>(&file, &matches);
     } else {
         symbolicate::<gimli::BigEndian>(&file, &matches);
@@ -29,12 +30,12 @@ fn parse_uint_from_hex_string(string: &str) -> u64 {
     }
 }
 
-fn entry_offsets_for_addresses<Endian>(file: &obj::File,
+fn entry_offsets_for_addresses<Endian>(file: &object::File,
                                        addrs: &Vec<u64>)
                                        -> Vec<Option<gimli::DebugInfoOffset>>
     where Endian: gimli::Endianity
 {
-    let aranges = obj::get_section(file, ".debug_aranges")
+    let aranges = object::get_section(file, ".debug_aranges")
         .expect("Can't addr2line with no aranges");
     let aranges = gimli::DebugAranges::<Endian>::new(aranges);
     let mut aranges = aranges.aranges();
@@ -86,19 +87,19 @@ fn display_file<Endian>(row: gimli::LineNumberRow<Endian>)
     }
 }
 
-fn symbolicate<Endian>(file: &obj::File, matches: &getopts::Matches)
+fn symbolicate<Endian>(file: &object::File, matches: &getopts::Matches)
     where Endian: gimli::Endianity
 {
     let addrs: Vec<u64> = matches.free.iter().map(|x| parse_uint_from_hex_string(x)).collect();
 
     let offsets = entry_offsets_for_addresses::<Endian>(&file, &addrs);
-    let debug_info = obj::get_section(file, ".debug_info")
+    let debug_info = object::get_section(file, ".debug_info")
         .expect("Can't addr2line without .debug_info");
     let debug_info = gimli::DebugInfo::<Endian>::new(debug_info);
-    let debug_abbrev = obj::get_section(&file, ".debug_abbrev")
+    let debug_abbrev = object::get_section(&file, ".debug_abbrev")
         .expect("Can't addr2line without .debug_abbrev");
     let debug_abbrev = gimli::DebugAbbrev::<Endian>::new(debug_abbrev);
-    let debug_line = obj::get_section(file, ".debug_line")
+    let debug_line = object::get_section(file, ".debug_line")
         .expect("Can't addr2line without .debug_line");
     let debug_line = gimli::DebugLine::<Endian>::new(&debug_line);
 
@@ -125,102 +126,6 @@ fn symbolicate<Endian>(file: &obj::File, matches: &getopts::Matches)
                     }
                 }
             }
-        }
-    }
-}
-
-// All cross platform / object file format compatibility stuff should
-// be contained in the `obj` module. Each supported platform / object
-// file format should implement the `obj` module with an identical
-// interface, but with the `pub type File` changing as needed. Hooray
-// duck typing!
-
-#[cfg(target_os="linux")]
-mod obj {
-    extern crate elf;
-    use std::path::Path;
-
-    /// The parsed object file type.
-    pub type File = elf::File;
-
-    /// Open and parse the object file at the given path.
-    pub fn open<P>(path: P) -> File
-        where P: AsRef<Path>
-    {
-        let path = path.as_ref();
-        elf::File::open_path(path).expect("Could not open file")
-    }
-
-    /// Get the contents of the section named `section_name`, if such
-    /// a section exists.
-    pub fn get_section<'a>(file: &'a File, section_name: &str) -> Option<&'a [u8]> {
-        file.sections
-            .iter()
-            .find(|s| s.shdr.name == section_name)
-            .map(|s| &s.data[..])
-    }
-
-    /// Return true if the file is little endian, false if it is big endian.
-    pub fn is_little_endian(file: &File) -> bool {
-        match file.ehdr.data {
-            elf::types::ELFDATA2LSB => true,
-            elf::types::ELFDATA2MSB => false,
-            otherwise => panic!("Unknown endianity: {}", otherwise),
-        }
-    }
-}
-
-#[cfg(target_os="macos")]
-mod obj {
-    extern crate mach_o;
-
-    use std::ffi::CString;
-    use std::fs;
-    use std::io::Read;
-    use std::mem;
-    use std::path::Path;
-
-    pub type File = Vec<u8>;
-
-    pub fn open<P>(path: P) -> File
-        where P: AsRef<Path>
-    {
-        let mut file = fs::File::open(path).expect("Could not open file");
-        let mut buf = Vec::new();
-        file.read_to_end(&mut buf).expect("Could not read file");
-        buf
-    }
-
-    // Translate the "." prefix to the "__" prefix used by OSX/Mach-O, eg
-    // ".debug_info" to "__debug_info".
-    fn translate_section_name(section_name: &str) -> CString {
-        let mut name = Vec::with_capacity(section_name.len() + 1);
-        name.push(b'_');
-        name.push(b'_');
-        for ch in &section_name.as_bytes()[1..] {
-            name.push(*ch);
-        }
-        unsafe { CString::from_vec_unchecked(name) }
-    }
-
-    pub fn get_section<'a>(file: &'a File, section_name: &str) -> Option<&'a [u8]> {
-        let parsed = mach_o::Header::new(&file[..]).expect("Could not parse macho-o file");
-
-        let segment_name = CString::new("__DWARF").unwrap();
-        let section_name = translate_section_name(section_name);
-        parsed.get_section(&segment_name, &section_name).map(|s| s.data())
-    }
-
-    pub fn is_little_endian(file: &File) -> bool {
-        let parsed = mach_o::Header::new(&file[..]).expect("Could not parse macho-o file");
-
-        let bytes = [1, 0, 0, 0u8];
-        let int: u32 = unsafe { mem::transmute(bytes) };
-        let native_byteorder_is_little = int == 1;
-
-        match (native_byteorder_is_little, parsed.is_native_byteorder()) {
-            (true, b) => b,
-            (false, b) => !b,
         }
     }
 }
