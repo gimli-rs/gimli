0b436d2f04b0cd16706b24897f7a64593c7bd2b2
Philip Craig <philipjcraig@gmail.com>
2017-06-26 17:38:49 +1000

    Implement Reader for EndianBuf

--- src/endianity.rs
+++ src/endianity.rs
@@ -7,7 +7,10 @@ use std::fmt::Debug;
 use std::io;
 use std::io::Read;
 use std::marker::PhantomData;
+use std::mem;
 use std::ops::{Deref, Index, Range, RangeFrom, RangeTo};
+use parser::{Error, Result};
+use reader::Reader;
 
 /// A trait describing the endianity of some buffer.
 ///
@@ -171,6 +174,17 @@ impl<'input, Endian> EndianBuf<'input, Endian>
     pub fn to_string_lossy(&self) -> Cow<'input, str> {
         String::from_utf8_lossy(self.buf)
     }
+
+    #[inline]
+    fn read_slice(&mut self, len: usize) -> Result<&'input [u8]> {
+        if self.buf.len() < len {
+            Err(Error::UnexpectedEof)
+        } else {
+            let val = &self.buf[..len];
+            self.buf = &self.buf[len..];
+            Ok(val)
+        }
+    }
 }
 
 /// # Range Methods
@@ -280,6 +294,122 @@ impl<'input, Endian> Read for EndianBuf<'input, Endian>
         buf[..len].copy_from_slice(&self.buf[..len]);
         self.buf = &self.buf[len..];
         Ok(len)
+    }
+}
+
+impl<'input, Endian> Reader for EndianBuf<'input, Endian>
+    where Endian: Endianity
+{
+    type Endian = Endian;
+
+    #[inline]
+    fn len(&self) -> usize {
+        self.buf.len()
+    }
+
+    #[inline]
+    fn is_empty(&self) -> bool {
+        self.buf.is_empty()
+    }
+
+    #[inline]
+    fn empty(&mut self) {
+        self.buf = &[];
+    }
+
+    #[inline]
+    fn truncate(&mut self, len: usize) -> Result<()> {
+        if self.buf.len() < len {
+            Err(Error::UnexpectedEof)
+        } else {
+            self.buf = &self.buf[..len];
+            Ok(())
+        }
+    }
+
+    #[inline]
+    fn offset_from(&self, base: &Self) -> usize {
+        self.offset_from(*base)
+    }
+
+    #[inline]
+    fn find(&self, byte: u8) -> Option<usize> {
+        self.find(byte)
+    }
+
+    #[inline]
+    fn skip(&mut self, len: usize) -> Result<()> {
+        if self.buf.len() < len {
+            Err(Error::UnexpectedEof)
+        } else {
+            self.buf = &self.buf[len..];
+            Ok(())
+        }
+    }
+
+    #[inline]
+    fn split(&mut self, len: usize) -> Result<Self> {
+        let slice = self.read_slice(len)?;
+        Ok(EndianBuf::new(slice))
+    }
+
+    #[inline]
+    fn read_u8_array<A>(&mut self) -> Result<A>
+        where A: Sized + Default + AsMut<[u8]>
+    {
+        let len = mem::size_of::<A>();
+        let slice = self.read_slice(len)?;
+        let mut val = Default::default();
+        <A as AsMut<[u8]>>::as_mut(&mut val).clone_from_slice(slice);
+        Ok(val)
+    }
+
+    #[inline]
+    fn read_u8(&mut self) -> Result<u8> {
+        let slice = self.read_slice(1)?;
+        Ok(slice[0])
+    }
+
+    #[inline]
+    fn read_i8(&mut self) -> Result<i8> {
+        let slice = self.read_slice(1)?;
+        Ok(slice[0] as i8)
+    }
+
+    #[inline]
+    fn read_u16(&mut self) -> Result<u16> {
+        let slice = self.read_slice(2)?;
+        Ok(Endian::read_u16(&slice))
+    }
+
+    #[inline]
+    fn read_i16(&mut self) -> Result<i16> {
+        let slice = self.read_slice(2)?;
+        Ok(Endian::read_i16(&slice))
+    }
+
+    #[inline]
+    fn read_u32(&mut self) -> Result<u32> {
+        let slice = self.read_slice(4)?;
+        Ok(Endian::read_u32(&slice))
+    }
+
+    #[inline]
+    fn read_i32(&mut self) -> Result<i32> {
+        let slice = self.read_slice(4)?;
+        Ok(Endian::read_i32(&slice))
+    }
+
+    #[inline]
+    fn read_u64(&mut self) -> Result<u64> {
+        let slice = self.read_slice(8)?;
+        Ok(Endian::read_u64(&slice))
+    }
+
+    #[inline]
+    fn read_i64(&mut self) -> Result<i64> {
+        let slice = self.read_slice(8)?;
+        Ok(Endian::read_i64(&slice))
     }
 }
 
