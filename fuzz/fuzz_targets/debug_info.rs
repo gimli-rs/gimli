f831aa18459ea9863b2c98eb30354f9c794f6d7b
Nick Fitzgerald <fitzgen@gmail.com>
2020-05-13 15:34:34 -0700

    Introduce a fuzz target for `.debug_info` and `.debug_abbrev`

---
+++ fuzz/fuzz_targets/debug_info.rs
@@ -0,0 +1,26 @@
+#![no_main]
+
+use gimli::{
+    read::{DebugAbbrev, DebugInfo},
+    LittleEndian,
+};
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|sections: (Vec<u8>, Vec<u8>)| {
+    let (debug_abbrev, debug_info) = sections;
+    let debug_abbrev = DebugAbbrev::new(&debug_abbrev, LittleEndian);
+    let debug_info = DebugInfo::new(&debug_info, LittleEndian);
+
+    let mut units = debug_info.units();
+    while let Ok(Some(unit)) = units.next() {
+        if let Ok(abbrevs) = unit.abbreviations(&debug_abbrev) {
+            let mut cursor = unit.entries(&abbrevs);
+            while let Ok(Some((_delta, entry))) = cursor.next_dfs() {
+                let mut attrs = entry.attrs();
+                while let Ok(Some(_attr)) = attrs.next() {
+                    continue;
+                }
+            }
+        }
+    }
+});
